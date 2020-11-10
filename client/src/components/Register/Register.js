9dcd4a0bcbef603547ce7b312d208a005a5e1e14
Jason Heredia <heredijr@gmatc.matc.edu>
2020-11-10 16:50:09 -0600

    Completed Activity 8

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,34 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-const Register = () => {
+const Register = ({ authenticateUser }) => {
+    let history = useHistory();
     const [userData, setUserData] = useState({
         name: '',
         email: '',
         password: '',
         passwordConfirm: ''
     });
+const [errorData, setErrorData] = useState({ errors: null});
 
-    const { name, email, password, passwordConfirm } = userData;
 
-    const onChange = e => {
-        const { name, value } = e.target;
-        setUserData({
-            ...userData,
-            [name]: value
-        })
+const {name, email, password, passwordConfirm } = userData;
+const { errors } = errorData;
+
+const onChange = e => {
+    const { name, value } = e.target;
+    setUserData({
+        ...userData,
+        [name]: value
+    })
+}
+
+const registerUser = async () => {
+    if (password !== passwordConfirm) {
+        console.log('Passwords do not match');
     }
-
-    const register = async () => {
-        if (password !== passwordConfirm) {
-            console.log('Password do not match');
+    else {
+        const newUser = {
+            name: name,
+            email: email,
+            password: password
         }
-        else {
-            const newUser = {
-                name: name,
-                email: email, 
-                password: password
-            }
 
         try {
             const config = {
@@ -39,54 +44,67 @@ const Register = () => {
 
             const body = JSON.stringify(newUser);
             const res = await axios.post('http://localhost:5000/api/users', body, config);
-            console.log(res.data);
+            
+            // Store user data and redirect
+            localStorage.setItem('token', res.data.token);
+            history.push('/');
         } catch (error) {
-            console.error(error.response.data);
-            return;
+            // Clear user data and set errors
+            localStorage.removeItem('token');
+            
+            setErrorData({
+                ...errors,
+                errors: error.response.data.errors
+            })
         }
-      }
-   }
 
-   return (
-       <div>
-           <h2>Register</h2>
-           <div>
-               <input
-                type="text"
-                placeholder="Name"
-                name="name"
-                value={name}
-                onChange={e => onChange(e)} />
-           </div>
-           <div>
-               <input 
+        authenticateUser();
+    }
+}
+
+return (
+    <div>
+        <h2>Register</h2>
+        <div>
+            <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={e => onChange(e)}/>
+        </div>
+        <div>
+            <input
                 type="text"
                 placeholder="Email"
                 name="email"
                 value={email}
-                 onChange={e => onChange(e)} />
-           </div>
-           <div>
-               <input
+                onChange={e => onChange(e)} />
+                </div>
+                <div>
+                    <input
                 type="text"
                 placeholder="Password"
                 name="password"
                 value={password}
                 onChange={e => onChange(e)} />
-           </div>
-           <div>
-               <input
+                </div>
+                <div>
+                    <input
                 type="text"
                 placeholder="Confirm Password"
                 name="passwordConfirm"
                 value={passwordConfirm}
                 onChange={e => onChange(e)} />
-           </div>
-           <div>
-               <button onClick={() => register()}>Register</button>
-           </div>
-       </div>
-   )
+                </div>
+                <div>
+                    <button onClick={() => registerUser()}>Register</button>
+        </div>
+        <div>
+            {errors && errors.map(error =>
+            <div key={error.msg}>{error.msg}</div>)}
+        </div>
+    </div>
+)
 }
-
 export default Register;
